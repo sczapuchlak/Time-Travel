d1151479b8c19b466ff7d742efa09507012568fc
Stephanie <yourname@stephanie.com>
2017-04-05 15:18:40 -0500

    color change

--- TimeTravel/Form1.Designer.cs
+++ TimeTravel/Form1.Designer.cs
@@ -97,6 +97,7 @@
             this.btnExit.TabIndex = 5;
             this.btnExit.Text = "E&xit";
             this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // Form1
             // 
--- TimeTravel/Form1.cs
+++ TimeTravel/Form1.cs
@@ -43,5 +43,10 @@ namespace TimeTravel
         {
 
         }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
